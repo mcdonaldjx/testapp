4df0bc7864280547f4abfe86953f8a6fefacf75e
Jared McDonald <17778094+mcdonaldjx@users.noreply.github.com>
2022-01-06 09:27:57 -0600

    Add files via upload

---
+++ src/testapp/Rectangle.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author asteed
+ */
+public class Rectangle implements Shape {
+    private final double base;
+    private final double height;
+    
+    Rectangle(double [] sides) {
+        checkForValidSides(sides);
+        this.base = sides[0];
+        this.height = sides[1];
+    }
+
+    private void checkForValidSides(double [] sides) {
+        if (sides.length != 2) {
+            throw new IllegalArgumentException("Invalid number of sides");
+        }
+
+        for (int i=0; i<sides.length; i++) {
+            if (sides[i] <= 0) {
+                throw new IllegalArgumentException("Invalid sides values");
+            }
+        }
+    }
+    
+    @Override
+    public double getCircumference() {
+        return 2 * base + 2 * height;
+    }
+
+    @Override
+    public double getArea() {
+        return base * height;
+    }
+    
+    public boolean isSquare() {
+        return base == height;
+    }
+}
